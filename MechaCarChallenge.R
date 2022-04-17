a37e09c0b58944aada2bda0ede7d840255954653
Anzhelika Suchkova <anzhelika.y.suchkova@gmail.com>
2022-04-16 21:22:56 -0600

    Add files via upload

---
+++ MechaCarChallenge.R
@@ -0,0 +1,4 @@
+library(dplyr)
+MechaCar_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_table) #create linear model
+summary(lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_table)) #summarize linear model
